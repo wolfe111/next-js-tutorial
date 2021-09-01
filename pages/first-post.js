a286b25ddc804b010d3af7e07e40a40e983c8849
jackw <jackw@motabilityoperations.co.uk>
2021-09-01 08:20:38 +0100

    First commit
    Moving laptops

---
+++ pages/first-post.js
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import Image from 'next/image'
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <div>
+          <Image
+            src="/images/profile.jpeg"
+            width={144}
+            height={144}
+            alt="Your profile"
+          />
+        </div>
+
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
+}
---
+++ pages/posts/[id].js
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import Layout from '../../components/layout'
+import {getAllPostIds, getPostData} from "../../lib/posts";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params}) {
+  const postData = await getPostData(params.id)
+
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default function Post({postData}) {
+  return (
+    <Layout>
+      {postData.title}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <br/>
+      {postData.id}
+      <br/>
+      {postData.date}
+      <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+    </Layout>
+  )
+}
