1f53c0c10cac5005fd0145c09114ec9cfbfe28eb
jackw <jackw@motabilityoperations.co.uk>
2021-09-16 18:32:56 +0100

    Edit first post again

--- pages/first-post.js
+++ pages/first-post.js
@@ -8,7 +8,7 @@ export default function FirstPost() {
   return (
     <Layout>
       <Head>
-        <title>First Post 2</title>
+        <title>First Post 3</title>
       </Head>
       <h1>First Post</h1>
       <h2>
